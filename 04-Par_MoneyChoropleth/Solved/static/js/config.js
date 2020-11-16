cb674133771cc69caf7fc817557a7992ae856d9c
William M Davis <williammdavis@Williams-MBP-2.fios-router.home>
2020-11-15 22:21:36 -0500

    label

---
+++ 04-Par_MoneyChoropleth/Solved/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiaGVyYW5tYW5lIiwiYSI6ImNrZ2gzeWRueTByZXkydXMyNGRsYmlvZncifQ.RmSFUf5o1Xaoiuz94H9-EA"
---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+// API key
+const API_KEY = "pk.eyJ1IjoiaGVyYW5tYW5lIiwiYSI6ImNrZ2gzeWRueTByZXkydXMyNGRsYmlvZncifQ.RmSFUf5o1Xaoiuz94H9-EA"
